39f7ce5c858e121867b44202f2ff0b67e51a6574
Hyh2001 <55273583+Hyh2001@users.noreply.github.com>
2022-03-03 16:42:55 +0800

    Update pid.h

---
+++ project_arm/pid.h
@@ -0,0 +1,47 @@
+#ifndef PID_H
+#define PID_H
+
+
+class PID_Controller{
+private:
+    float KI;
+    float KP;
+    float KD;
+    float error_now;
+    float error_pre;
+    float integral;
+    float integral_max;
+    float output_max;
+
+public:
+    void set_error(float error){
+        error_now = error;
+    }
+    void set_error_pre(float error){
+        error_pre = error;
+    }
+    float get_error_now(){
+        return error_now;
+    }
+    float get_error_pre(){
+        return error_pre;
+    }
+    float get_KD(){
+        return KD;
+    }
+    float get_KP(){
+        return KP;
+    }
+    float get_max(){
+        return output_max;
+    }
+    float start_integral(){
+
+    }
+
+};
+
+
+
+
+#endif
