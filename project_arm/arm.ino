4b3841737863a5a98afee509b475c51e5b46e917
Hyh2001 <55273583+Hyh2001@users.noreply.github.com>
2022-03-12 14:27:36 +0800

    servo

--- project_arm/arm.ino
+++ project_arm/arm.ino
@@ -1,20 +1,30 @@
+#include <SoftwareSerial.h> 
 #include "pid.h"
 #include <Servo.h>
 #include <Wire.h>
+#include "PS2X_lib.h"                     //调用库文件PS2X_lib，这个是无线手柄库
+#include "BTJoystick.h" 
+
 
 Servo servo_base;
 Servo servo_big_arm;
 Servo servo_small_arm;
 Servo servo_catch;
+
+
 PID_Controller pid_base;
 PID_Controller pid_big_arm;
 PID_Controller pid_small_arm;
 PID_Controller pid_catch;
 void setup(){
-  servo_base.attach(1);
+  Serial.begin( 57600 );  //串口通讯波特率57600
+  softSerial.begin(9600); //软串口通讯开启波特率9600，此项设置必须与蓝牙模块串口波特率一致
+  softSerial.listen();    //开启软串口数据监听
+  delay( 2000 );  
+  servo_base.attach(3);
   servo_big_arm.attach(2);
-  servo_small_arm.attach(3);
-  servo_catch.attach(4);
+  servo_small_arm.attach(1);
+  servo_catch.attach(0);
 
 
 }
@@ -39,4 +49,10 @@ float PID_cal(PID_Controller* pid_x, int Now_value, int Aim_value, char flag){  
   output = output > -pid_x-> get_max() ? -pid_x-> get_max() : output;
   pid_x->set_error_pre(error);
   return output;
+}
+
+void record(){    // is used to record the previous state when xxx button pressed
+
+
+
 }
